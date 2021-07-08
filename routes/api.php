67cde1b0babb317ec9018d89f8c7e24434f41ccb
alifdelgado <alifdelgado@gmail.com>
2021-07-08 03:09:19 -0500

    Going to green with the first tests about contacts

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::post('/contacts', [ContactsController::class, 'store'])->name('contacts.store');
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
--- tests/Feature/ContactsTest.php
+++ tests/Feature/ContactsTest.php
@@ -2,10 +2,61 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Contact;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ContactsTest extends TestCase
 {
+    use RefreshDatabase;
+
+    /**
+     * @test
+     */
+    public function a_contact_can_be_added()
+    {
+        $this->withoutExceptionHandling();
+        $this->post('/api/contacts', $this->userValidData());
+        $contact = Contact::first();
+        $this->assertEquals('Test name', $contact->name);
+        $this->assertEquals('test@email.com', $contact->email);
+        $this->assertEquals('05/14/1988', $contact->birthday);
+        $this->assertEquals('ABC Development', $contact->company);
+    }
+
+    /**
+     * @test
+     */
+    public function a_name_is_required()
+    {
+        $this->post('/api/contacts',
+                $this->userValidData(['name' => null]))
+            ->assertSessionHasErrors('name')
+            ->assertCount(0, Contact::all());
+    }
+
+    /**
+     * @test
+     */
+    public function an_email_is_required()
+    {
+        $this->post('/api/contacts',
+                $this->userValidData(['email' => null]))
+            ->assertSessionHasErrors('email')
+            ->assertCount(0, Contact::all());
+    }
+
+     /**
+     * @return array
+     */
+    protected function userValidData($overrides = []): array
+    {
+        return array_merge([
+            'name'      => 'Test name',
+            'email'     => 'test@email.com',
+            'birthday'  => '05/14/1988',
+            'company'   => 'ABC Development'
+        ], $overrides);
+    }
 }
